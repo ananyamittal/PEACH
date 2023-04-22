75190846af6d831dec38f64bc1a285f4f56277c4
Ashmeet Kaur <103422145+ashmeet0906@users.noreply.github.com>
2023-04-22 15:23:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ import numpy as np
 
 from keras.models import load_model
 model = load_model('model.h5')
+import json
+import random
+intents = json.loads(open('data.json').read())
+words = pickle.load(open('texts.pkl','rb'))
+classes = pickle.load(open('labels.pkl','rb'))
