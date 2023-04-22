73966c070fd710f19729a567fb2a4a9b196d06ea
Ashmeet Kaur <103422145+ashmeet0906@users.noreply.github.com>
2023-04-22 13:28:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import nltk
+nltk.download('popular')
+from nltk.stem import WordNetLemmatizer 
+lemmatizer = WordNetLemmatizer()
+import pickle 
+import numpy as np 
 
+from keras.models import load_model
+model = load_model('model.h5')
