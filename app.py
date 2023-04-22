66e688a51eb3bf18dbc6a8bf737196e349e12099
Ashmeet Kaur <103422145+ashmeet0906@users.noreply.github.com>
2023-04-22 15:27:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,3 +67,11 @@ app.static_folder = 'static'
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return chatbot_response(userText)
