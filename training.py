c14d0d5285983138bdf0c0110da574a9aa04613a
ananyamittal <105580962+ananyamittal@users.noreply.github.com>
2023-04-22 11:21:03 +0530

    Update training.py

--- training.py
+++ training.py
@@ -1,1 +1,15 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+lematizer = WordNetLEmatizer()
+import json
+import pickle
 
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
+
+words=[]
+classes=[]
+documents=[]
