6499fccc997634b1543a0a1ddc928854a162684a
Ashmeet Kaur <103422145+ashmeet0906@users.noreply.github.com>
2023-04-22 15:26:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,3 +40,14 @@ def predict_class(sentence, model):
     for r in results:
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     return return_list
+    
+    
+    
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
