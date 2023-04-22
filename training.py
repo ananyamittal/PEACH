adc16940cea85a66f531ab35a8354cc3ecd23476
ananyamittal <105580962+ananyamittal@users.noreply.github.com>
2023-04-22 14:07:15 +0530

    Update training.py

--- training.py
+++ training.py
@@ -34,3 +34,22 @@ classes = sorted(list(set(classes)))
 print (len(documents), "documents")
 print (len(classes), "classes", classes)
 print (len(words), "unique lemmatized words", words)
+
+pickle.dump(words,open('texts.pkl','wb'))
+pickle.dump(classes,open('labels.pkl','wb'))
+
+training = []
+output_empty = [0] * len(classes)
+for doc in documents:
+    bag = []
+    pattern_words = doc[0]
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+    
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+    
+    training.append([bag, output_row])
+random.shuffle(training)
+training = np.array(training)
