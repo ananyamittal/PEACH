f3d30c93f738c5a35b157e1bf80625d8cd380c97
Ashmeet Kaur <103422145+ashmeet0906@users.noreply.github.com>
2023-04-22 15:24:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,14 @@ def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
+
+def bow(sentence, words, show_details=True):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
